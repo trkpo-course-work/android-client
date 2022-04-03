b4ed35370ec106bcae2df372bb55c76ee776cbb8
Artur <a.nurtdinow@yandex.ru>
2022-04-03 20:24:12 +0300

    Fix Profile Edit test

--- app/src/androidTest/java/ru/spbstu/blog/root/presentation/ProfileEditTest.kt
+++ app/src/androidTest/java/ru/spbstu/blog/root/presentation/ProfileEditTest.kt
@@ -374,14 +374,19 @@ class ProfileEditTest {
     }
 
     companion object {
-        private val ALLOWED_CHARACTERS = "qwertyuiopasdfghjklzxcvbnm "
+        private val ALLOWED_CHARACTERS = "qwertyuiopasdfghjklzxcvbnm"
     }
 
     private fun getRandomString(sizeOfRandomString: Int): String {
         val random = Random()
         val sb = StringBuilder(sizeOfRandomString)
-        for (i in 0 until sizeOfRandomString)
-            sb.append(ALLOWED_CHARACTERS[random.nextInt(ALLOWED_CHARACTERS.length)])
+        for (i in 0 until sizeOfRandomString) {
+            if (i == 7) {
+                sb.append(" ")
+            } else {
+                sb.append(ALLOWED_CHARACTERS[random.nextInt(ALLOWED_CHARACTERS.length)])
+            }
+        }
         return sb.toString()
     }
 
